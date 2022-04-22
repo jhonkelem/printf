4817061551138f35c05ee5e758772713f5d4f1f7
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-04-23 00:00:17 +0200

    Create _print_char.c

---
+++ _print_char.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _print_char - prints characters
+ * @c: The character
+ *
+ * Void: The number of characters that were printed
+ */
+int _print_char(char c)
+{
+  _return (_putchar(c));
+}
