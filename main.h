6dd3e481f0716f697c671ac508bd2086d557dbb8
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-04-23 00:29:10 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int _print_char(char c);
-int _print_int(int n);
-int _print_str(char *s);
+int _print_char(va_list args);
+int _print_int(va_list args);
+int _print_str(va_list args);
 
 #endif
