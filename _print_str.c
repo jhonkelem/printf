991c78fd089de59d73ffd7c82fde8cbd0bbd60da
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-04-23 00:07:34 +0200

    Update _print_str.c

--- _print_str.c
+++ _print_str.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * _print_str - produces output according to a format
+ * @s: The string
+ *
+ * Return: The number of characters that were printed
+ */
+int _print_str(char *s)
+{
+  int k = 0;
+  
+  while (str[k] != '\0')
+	{
+    _putchar(str[k]);
+    k++;
+	}
+  return (0);
+}
