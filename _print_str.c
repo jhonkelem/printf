534e5b3daa1bb3d75ddcee6f65ceac99ed0d5a2d
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-04-23 00:24:15 +0200

    Update _print_str.c

--- _print_str.c
+++ _print_str.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
  * _print_str - produces output according to a format
@@ -6,9 +7,10 @@
  *
  * Return: The number of characters that were printed
  */
-int _print_str(char *s)
+int _print_str(va_list args)
 {
   int k = 0;
+  char *str = va_arg(args, char *);
   
   while (str[k] != '\0')
   {
